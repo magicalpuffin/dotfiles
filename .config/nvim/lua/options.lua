9ceb098ce08574cdb8a5e75d7d0ef09a364167e8
Michael Li <mealted.lemon@gmail.com>
2024-06-02 10:09:25 -0700

    init nvim config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,7 @@
+require "nvchad.options"
+
+local o = vim.o
+
+o.cursorlineopt = "both"
+o.shell = "fish"
+o.relativenumber = true
---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "tailwindcss-language-server",
+        "rust-analyzer",
+        "ruff",
+        "eslint-lsp",
+        "prettier",
+        "biome",
+        "typescript-language-server",
+        "svelte-language-server",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "vimdoc",
+        "lua",
+        "bash",
+        "markdown",
+        "html",
+        "css",
+        "toml",
+        "yaml",
+        "json",
+        "sql",
+        "rust",
+        "python",
+        "javascript",
+        "typescript",
+        "svelte",
+      },
+    },
+  },
+}
