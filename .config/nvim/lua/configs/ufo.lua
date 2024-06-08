646e505671e8c2c5c53f8150b22d407150a40998
Michael Li <mealted.lemon@gmail.com>
2024-06-07 21:34:40 -0700

    Added ufo hotkeys

--- .config/nvim/lua/configs/ufo.lua
+++ .config/nvim/lua/configs/ufo.lua
@@ -2,8 +2,17 @@ vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+local kinds = { "comment", "imports" }
+
 require("ufo").setup {
-  close_fold_kinds_for_ft = {
-    default = { "comment" },
-  },
+  -- close_fold_kinds_for_ft = {
+  --   default = { "comment", "imports" },
+  -- },
 }
+
+vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+vim.keymap.set("n", "zr", function()
+  require("ufo").openFoldsExceptKinds(kinds)
+end)
+vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
