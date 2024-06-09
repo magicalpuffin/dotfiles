27a0e4a1567101a5b56494719c9bc2fd5cb085dc
Michael Li <mealted.lemon@gmail.com>
2024-06-09 12:42:12 -0700

    added fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set VOLTA_HOME $HOME/.volta
+
+# Created by `pipx` on 2023-12-01 04:38:22
+set PATH $VOLTA_HOME/bin $PATH /home/magicalpuffin/.local/bin ~/.cargo/bin/ ~/.npm-global/bin 
