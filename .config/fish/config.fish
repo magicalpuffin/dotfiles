299f0f60c7dfadff2a552e7b4b651dc2278c901c
Michael Li <mealted.lemon@gmail.com>
2024-08-15 19:27:13 -0700

    cleanup set path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,15 @@ end
 
 alias pn=pnpm
 
-set VOLTA_HOME $HOME/.volta
-
 # Created by `pipx` on 2023-12-01 04:38:22
-set PATH $VOLTA_HOME/bin $PATH /home/magicalpuffin/.local/bin ~/.cargo/bin/ ~/.npm-global/bin 
+set PATH $PATH /home/magicalpuffin/.local/bin
 
+# cargo
+set PATH $PATH ~/.cargo/bin/ ~/.npm-global/bin
+
+# volta
+set VOLTA_HOME $HOME/.volta
+set PATH $PATH $VOLTA_HOME/bin
+
+# turso
 set PATH $PATH /home/magicalpuffin/.turso/
