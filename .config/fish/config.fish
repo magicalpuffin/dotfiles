9d311bb8a7e318961c5b4044772405ab1558400f
Michael Li <mealted.lemon@gmail.com>
2024-08-15 19:19:53 -0700

    added turso path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,5 @@ set VOLTA_HOME $HOME/.volta
 
 # Created by `pipx` on 2023-12-01 04:38:22
 set PATH $VOLTA_HOME/bin $PATH /home/magicalpuffin/.local/bin ~/.cargo/bin/ ~/.npm-global/bin 
+
+set PATH $PATH /home/magicalpuffin/.turso/
