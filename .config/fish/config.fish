bd67bc7e6f386a8ceaad13977beb209fed246eec
Michael Li <mealted.lemon@gmail.com>
2024-08-28 19:57:00 -0700

    update to use tilde for home

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ end
 alias pn=pnpm
 
 # Created by `pipx` on 2023-12-01 04:38:22
-set PATH $PATH /home/magicalpuffin/.local/bin
+set PATH $PATH ~/.local/bin
 
 # cargo
 set PATH $PATH ~/.cargo/bin/ ~/.npm-global/bin
@@ -15,4 +15,4 @@ set VOLTA_HOME $HOME/.volta
 set PATH $PATH $VOLTA_HOME/bin
 
 # turso
-set PATH $PATH /home/magicalpuffin/.turso/
+set PATH $PATH ~/.turso/
